224a4fec8014173cbf844a819aab8339bb9dc85a
elsaneth <elisabeth.suits@gmail.com>
2024-05-10 17:02:13 +0300

    2 users can their moves properly (no automatic computer move yet)

--- src/main/java/TicTacToe/GameApp.java
+++ src/main/java/TicTacToe/GameApp.java
@@ -1,4 +1,4 @@
-package org.example.oop6tictactore;
+package TicTacToe;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,16 +6,20 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class GameApp extends Application {
     private char currentPlayer;
     private char computerPlayer;
-
+    private boolean isComputerTurn;
+    String player = "your";
+    Label gameInstructionsTitle = new Label("It's your turn.");
     @Override
     public void start(Stage stage) throws IOException {
         VBox root = new VBox(10); // Vertical layout with 10px spacing
@@ -55,12 +59,22 @@ public class GameApp extends Application {
 
         chooseX.setOnAction(event -> {
             System.out.println("Player X button is clicked");
+            try {
+                gameBoardView(stage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             currentPlayer = 'X';
             computerPlayer = 'O';
         });
 
         chooseO.setOnAction(event -> {
             System.out.println("Player O button is clicked");
+            try {
+                gameBoardView(stage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             currentPlayer = 'O';
             computerPlayer = 'X';
         });
@@ -77,8 +91,50 @@ public class GameApp extends Application {
     }
 
     public void gameBoardView(Stage stage) throws IOException {
+        gameInstructionsTitle.setStyle("-fx-font-size: 18px;");
+        GridPane gameBoard = new GridPane();
+        gameBoard.setAlignment(Pos.CENTER);
+        isComputerTurn = false;
 
+        for (int row = 0; row < 4; row ++) {
+            for (int col = 0; col < 4; col++) {
+                Button cell = new Button();
+                cell.setMinSize(50, 50);
+                cell.setOnAction(event -> {
+                    if (cell.getText().isEmpty()) {
+                        fillCell(cell);
+                    }
+                });
+                gameBoard.add(cell, row, col);
+            }
+        }
+
+        VBox root = new VBox(10, gameInstructionsTitle, gameBoard);
+        root.setAlignment(Pos.CENTER);
+
+        Scene gameBoardScene = new Scene(root, 320, 240);
+        stage.setScene(gameBoardScene);
+        stage.show();
     }
+
+    private void fillCell(Button cell) {
+        System.out.println("Fill cell action user");
+        cell.setText(String.valueOf(currentPlayer));
+        // toggles between X and O player
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+
+        if (currentPlayer == computerPlayer) {
+            gameInstructionsTitle.setText("It's computer's turn.");
+        } else {
+            gameInstructionsTitle.setText("It's your turn.");
+        }
+    }
+
+    private void makeComputerTurn(Button cell) {
+        System.out.println("Fill cell action computer");
+        cell.setText(String.valueOf(computerPlayer));
+    }
+
 
     public static void main(String[] args) {
         launch();
