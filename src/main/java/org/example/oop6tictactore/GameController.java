e43e1b36bc901c6fdc79b134abd844e2afd91333
elsaneth <elisabeth.suits@gmail.com>
2024-05-10 14:18:16 +0300

    deleted (not using)

--- src/main/java/org/example/oop6tictactore/GameController.java
+++ src/main/java/org/example/oop6tictactore/GameController.java
@@ -10,24 +10,5 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class GameController {
-    public Label welcomeText;
-    private  Stage stage;
 
-    @FXML
-    protected void onStartGameButton() {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("choosePlayerView.fxml"));
-            Parent root = fxmlLoader.load();
-            Scene newScene = new Scene(root, 320, 240);
-
-            stage.setScene(newScene); // Change the scene on the stage
-            stage.show(); // Display the new scene
-        } catch (IOException e) {
-            e.printStackTrace(); // Handle any exceptions during scene loading
-        }
-    }
-
-    public void setStage(Stage stage) {
-        this.stage = stage; // Set the stage reference
-    }
 }
