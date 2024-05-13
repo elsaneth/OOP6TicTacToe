acaf254f7f3777d596b59f33676775eb26ba08a8
elsaneth <elisabeth.suits@gmail.com>
2024-05-13 23:10:13 +0300

    whole project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.oop6tictactore {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens TicTacToe to javafx.fxml;
+    exports TicTacToe;
+}
